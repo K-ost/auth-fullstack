63dc703c8fd85717cfc4abb0c42f30ffe1a8036f
K-ost <golubkovru@gmail.com>
2026-02-21 21:59:43 +0200

    auth middleware for verifying access token

---
+++ server/src/middleware/auth-middleware.ts
@@ -0,0 +1,26 @@
+import { NextFunction, Request, Response } from "express";
+import jwt, { JwtPayload } from "jsonwebtoken";
+
+interface AuthRequest extends Request {
+  user?: JwtPayload | string;
+}
+
+function authMiddleware(req: AuthRequest, res: Response, next: NextFunction) {
+  try {
+    const authorization = req.headers.authorization;
+    if (!authorization) return res.sendStatus(401);
+
+    const token = authorization.split(" ")[1];
+    if (!token) return res.sendStatus(401);
+
+    jwt.verify(token, process.env.ACCESS_TOKEN as string, (err, data) => {
+      if (err) return res.sendStatus(401);
+      req.user = data;
+      next();
+    });
+  } catch (error) {
+    res.sendStatus(500);
+  }
+}
+
+export default authMiddleware;
--- server/src/routes/Users.ts
+++ server/src/routes/Users.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import usersController from "../controllers/usersController";
+import authMiddleware from "../middleware/auth-middleware";
 
 const usersRouter = Router();
 
-usersRouter.get("/users", usersController.getUsers);
+usersRouter.get("/users", authMiddleware, usersController.getUsers);
 usersRouter.get("/users/:id", usersController.getUserById);
 usersRouter.patch("/users/:id", usersController.editUserById);
 usersRouter.delete("/users/:id", usersController.deleteUserById);
